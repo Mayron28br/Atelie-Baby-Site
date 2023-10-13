5fd494de41c33dc6dcf5fc69f7a218fd96dd736c
Mayron28br <mayronwilke28@gmail.com>
2023-10-13 06:06:28 -0300

    AHAHHAHAHAHAH

--- bolsa.js
+++ bolsa.js
@@ -9,11 +9,3 @@ buttonclose.onclick = function () {
     modal.close();
 };
 
-document.getElementById('openPopup').addEventListener('click', function () {
-    document.getElementById('popup').style.display = 'flex';
-  });
-  
-  document.getElementById('closePopup').addEventListener('click', function () {
-    document.getElementById('popup').style.display = 'none';
-  });
-  
