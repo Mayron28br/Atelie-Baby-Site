d615070e202231de1cede8d0c3efc9761057dcfc
Lucas Henrique <lucashenriquesramos@gmail.com>
2023-10-24 13:43:25 -0300

    botao (inicio, produtos) v-1.0

--- HTML/JavaScript/bolsa.js
+++ HTML/JavaScript/bolsa.js
@@ -9,3 +9,39 @@ buttonclose.onclick = function () {
     modal.close();
 };
 
+/*Botao Topo*/
+
+function checkScrollPosition() {
+    var botaotopo = document.getElementById("botaotopo");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        botaotopo.classList.add("active");
+    } else {
+        botaotopo.classList.remove("active");
+    }
+}
+
+window.onscroll = function() {
+    checkScrollPosition();
+}
+
+function subirbotao() {
+    var duration = 1000; // Aumentamos a duração para 1000ms para uma animação mais suave
+    var interval = 10; // Intervalo de atualização
+
+    var scrollStep = -window.scrollY / (duration / interval);
+
+    var scrollInterval = setInterval(function() {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, interval);
+}
+
+// Quando o botão é clicado, chame a função subirbotao
+document.getElementById("botaotopo").addEventListener("click", function() {
+    subirbotao();
+});
+
+/*FIM Botao Topo*/
--- HTML/JavaScript/inicio.js
+++ HTML/JavaScript/inicio.js
@@ -17,6 +17,39 @@ const swiper = new Swiper('.swiper', {
     
     });
 
-/*Animaçoes de visibilidade*/
+/*Botao Topo*/
 
-/*FIM animações de visibilidade*/
+function checkScrollPosition() {
+    var botaotopo = document.getElementById("botaotopo");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        botaotopo.classList.add("active");
+    } else {
+        botaotopo.classList.remove("active");
+    }
+}
+
+window.onscroll = function() {
+    checkScrollPosition();
+}
+
+function subirbotao() {
+    var duration = 1000; // Aumentamos a duração para 1000ms para uma animação mais suave
+    var interval = 10; // Intervalo de atualização
+
+    var scrollStep = -window.scrollY / (duration / interval);
+
+    var scrollInterval = setInterval(function() {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, interval);
+}
+
+// Quando o botão é clicado, chame a função subirbotao
+document.getElementById("botaotopo").addEventListener("click", function() {
+    subirbotao();
+});
+
+/*FIM Botao Topo*/
