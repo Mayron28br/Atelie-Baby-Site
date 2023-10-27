62df9279c7a4c7af244b26522ee1d44c402561ab
Mayron28br <mayronwilke28@gmail.com>
2023-10-27 03:45:04 -0300

    animação apenas uma vez home

--- animacoes.js
+++ animacoes.js
@@ -13,3 +13,17 @@ window.onscroll = () => {
         }
     })
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, {
+    rootMargin: '200px', // Área de ativação
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
