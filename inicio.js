47b43bb273f24965b3268586e6a61320f6131030
Mayron28br <mayronwilke28@gmail.com>
2023-10-31 10:30:56 -0300

    tentativa de fazer o carrinho

--- inicio.js
+++ inicio.js
@@ -63,5 +63,68 @@ function closeModal(modalId) {
     modal.style.display = "none";
 }
 
+/* Carrinho */
+
+let iconCart = document.querySelector('.carrinho');
+let closeCart = document.querySelector('.carrinho-botao-fechar');
+let section = document.querySelector('section');
+let listProductHTML = document.querySelector('.carrinho-lista');
+
+let listProduct = [];
+
+iconCart.addEventListener('mouseenter', () => {
+    section.classList.toggle('show-carrinho')
+});
+
+closeCart.addEventListener('click', () => {
+    section.classList.toggle('show-carrinho')
+});
 
 
+const addDataToHTML = () => {
+    listProductHTML.innerHTML = '';
+    if (listProduct.length > 0){
+        listProduct.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('item');
+            newProduct.innerHTML = `
+            <div class="card-produto-img"><a href="bolsa.html"><img src="${produtos.imagem}"></a></div>
+                <div class="card-produto-info">
+                  <a href="bolsa.html" class="card-produto-text-title">${produtos.name} </a>
+                  <p class="card-produto-text-body">Product of Lux</p>
+                </div>
+                <div class="card-produto-footer">
+                <span class="card-produto-text-title">${produtos.price}</span>
+                <div class="card-produto-button">
+                  <svg class="svg-icon" viewBox="0 0 20 20"><i class="fa-solid fa-cart-shopping"></i></svg>
+                </div>
+                </div>
+        `;
+        })
+    }
+}
+// Selecione todos os elementos com a classe 'card-produto-button'
+let buttons = document.querySelectorAll('.card-produto-button');
+
+// Adicione um evento de clique a cada botão
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        let product_id = event.target.closest('.item').dataset.id;
+        alert(product_id);
+    });
+});
+
+
+const initapp = () => {
+    //get data from json
+    fetch('produtos.json')
+    .then(response => response.json())
+    .then(data => {
+        listProduct = data;
+        addDataToHTML();
+    })
+}
+
+initapp();
+
+/*FIM carrinho*/
