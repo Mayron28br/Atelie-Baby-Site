d93452608236680530cac526d8244c6434923cfd
Franklin P. Barboza <139097233+FPGAMESBR@users.noreply.github.com>
2023-11-20 23:17:29 -0300

    Login/Registro

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -85,24 +85,113 @@ document.getElementById("signin-form").addEventListener("submit", (event) => {
   }
 });
 
-// ... (seu código atual) ...
+// Login/Registro
 
-function registerUser(email, password) {
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    const existingUser = users.find(user => user.email === email);
+document.addEventListener("DOMContentLoaded", function () {
+    const signinForm = document.getElementById("signin-form");
+    const signupForm = document.getElementById("signup-form");
   
-    if (existingUser) {
-      alert('Este email já está registrado.');
-      return;
-    }
+    // Função para mostrar uma mensagem de sucesso
+    const showSuccessMessage = (message) => {
+      const successMessage = document.createElement("div");
+      successMessage.className = "success-message";
+      successMessage.textContent = message;
+      document.body.appendChild(successMessage);
   
-    const newUser = { email, password };
-    users.push(newUser);
-    localStorage.setItem('users', JSON.stringify(users));
+      // Adiciona estilo para centralizar e arredondar a mensagem
+      successMessage.style.position = "fixed";
+      successMessage.style.top = "50%";
+      successMessage.style.left = "50%";
+      successMessage.style.transform = "translate(-50%, -50%)";
+      successMessage.style.backgroundColor = "#4CAF50";
+      successMessage.style.color = "white";
+      successMessage.style.padding = "20px";
+      successMessage.style.zIndex = "1";
+      successMessage.style.borderRadius = "50px"; // Adiciona bordas arredondadas
   
-    // Redirecionar para a página de clientes após o registro
-    window.location.href = 'clientes.html';
-  }
+      // Remover a mensagem após alguns segundos
+      setTimeout(() => {
+        document.body.removeChild(successMessage);
+      }, 3000);
+    };
   
-  // ... (seu código atual) ...
+    // Função para exibir mensagens de erro
+    const displayErrorMessage = (elementId, message) => {
+      const errorElement = document.getElementById(elementId);
+      errorElement.textContent = message;
+      errorElement.style.visibility = "visible";
+    };
+  
+    // Função para limpar mensagens de erro
+    const clearErrorMessages = () => {
+      const errorMessages = document.querySelectorAll(".error-message");
+      errorMessages.forEach((element) => {
+        element.textContent = "";
+        element.style.visibility = "hidden";
+      });
+    };
+  
+    // Função para verificar se o email já está registrado
+    const isEmailRegistered = (email) => {
+      const users = JSON.parse(localStorage.getItem("registeredUsers")) || [];
+      return users.some((user) => user.email === email);
+    };
+  
+    // Função para realizar o registro do usuário
+    const registerUser = (email, password) => {
+      if (isEmailRegistered(email)) {
+        displayErrorMessage("email-signup-error", "Este email já está registrado.");
+        return;
+      }
+  
+      const users = JSON.parse(localStorage.getItem("registeredUsers")) || [];
+      users.push({ email, password });
+      localStorage.setItem("registeredUsers", JSON.stringify(users));
+  
+      document.getElementById("email-signup").value = "";
+      document.getElementById("password-signup").value = "";
+      document.getElementById("password-confirmation-signup").value = "";
+  
+      showSuccessMessage("Registro bem-sucedido! Você será redirecionado para a página de login.");
+  
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000);
+    };
+  
+    // Adiciona um ouvinte de eventos para o envio do formulário de registro
+    signupForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const email = document.getElementById("email-signup").value;
+      const password = document.getElementById("password-signup").value;
+      const passwordConfirmation = document.getElementById("password-confirmation-signup").value;
+  
+      if (password !== passwordConfirmation) {
+        displayErrorMessage("password-confirmation-signup-error", "As senhas não coincidem. Tente novamente.");
+        return;
+      }
+  
+      registerUser(email, password);
+    });
+  
+    // Adiciona um ouvinte de eventos para o envio do formulário de login
+    signinForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const email = document.getElementById("email-signin").value;
+      const password = document.getElementById("password-signin").value;
+  
+      const users = JSON.parse(localStorage.getItem("registeredUsers")) || [];
+  
+      const user = users.find((user) => user.email === email && user.password === password);
+  
+      if (user) {
+        showSuccessMessage("Login bem-sucedido! Você será redirecionado para a página inicial.");
+        setTimeout(() => {
+          window.location.href = "index.html";
+        }, 3000);
+      } else {
+        displayErrorMessage("email-signin-error", "Credenciais inválidas. Tente novamente.");
+      }
+    });
+  });
   
